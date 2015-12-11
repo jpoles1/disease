682a8d2219f5f16262749cfe4719546f50397136
Jordan Poles <jpoles1@gmail.com>
2015-12-11 14:54:56 -0600

    Got a lot of new stuff up and running. Very exciting!

--- model2.py
+++ model2.py
@@ -5,23 +5,28 @@ Created on Tue Dec  8 13:41:00 2015
 @author: jpoles1
 """
 import networkx as nx
+import random
+from forceatlas import forceatlas2_layout
 from networkx.readwrite import json_graph
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
+import os
 
 themeColors = {"alive": "green", "infected": "orange", "dead": "red", "recovered": "blue"}
 class World:
     def __init__(self, initPopulation):
         self.popsize = initPopulation;
         self.population = []
-        self.diseaseList = {};
+        self.diseaseList = [];
         self.age = 0;
         for indv in range(initPopulation):
             self.population.append(Person(self));
         self.worldgraph = nx.watts_strogatz_graph(initPopulation, 4,  .3);
         mappin = {num: per for (num, per) in enumerate(self.population)}
         nx.relabel_nodes(self.worldgraph, mappin, copy=False)
-        self.nodeLayout = nx.spring_layout(self.worldgraph)
+        #self.nodeLayout = nx.spring_layout(self.worldgraph, k=.01)
+        self.nodeLayout = forceatlas2_layout(self.worldgraph, iterations=100)
         nx.set_node_attributes(self.worldgraph, 'color', "green")
     def death(self, person):
         self.popsize-=1;
@@ -35,16 +40,26 @@ class World:
         self.worldgraph
     def draw(self):
         nodeColors = [x.color for x in nx.nodes_iter(self.worldgraph)]
-        nx.draw(self.worldgraph, layout=self.nodeLayout, node_color=nodeColors)
-        plt.savefig("graphseries/graph"+str(self.age)+".png")
+        plt.figure(figsize=(10,8))
+        plt.title("Network at Age "+str(self.age))
+        nx.draw(self.worldgraph, pos=self.nodeLayout, node_color=nodeColors, node_size=30, hold=1)
+        plt.savefig("graphseries/graph"+str(self.age).zfill(4)+".png")
         plt.close()
     def tick(self):
         self.age+=1;
         if(self.age%4 == 0):
             self.draw();
-        interactions = random.sample(self.worldgraph.edges(), self.popsize/4)
+        interactions = random.sample(self.worldgraph.edges(), self.popsize/6)
         for edge in interactions:
             edge[0].interact(edge[1])
+        for person in self.population:
+            pass
+        for disease in self.diseaseList:
+            print(disease.infected)
+            disease.history[self.age] = disease.infected;
+    def summary(self):
+        history = {disease.id : disease.history for disease in self.diseaseList}
+        return pd.DataFrame(history);
 class Town:
     pass
 class Person:
@@ -56,9 +71,11 @@ class Person:
         Person.idct+=1;
         self.resistances = [] #each entry like {id: 1, resist: .05}
         self.world = world;
+        self.alive = 1;
         self.color = themeColors["alive"]
     def infect(self, disease):
         self.diseases.append(disease);
+        disease.infected+=1;
         self.color = themeColors["infected"]
     def recover(self, disease, resist):
         try:
@@ -77,26 +94,34 @@ class Person:
                     if(test>resistance["resist"]):
                         b.infect(disease);
                         newInfections.append(disease.id)
+                    else:
+                        print("individual resisted infection!")
                 except:
                     b.infect(disease);
                     newInfections.append(disease.id)
         return newInfections
     def interact(self, otherActor):
-        print("Infections from A to B:", Person.checkDisease(self, otherActor))
-        print("Infections from B to A:", Person.checkDisease(otherActor, self))
+        if(self.alive==1 and otherActor.alive==1):
+            a = Person.checkDisease(self, otherActor);
+            if(len(a)>0):
+                print("Infections from A to B:", a)
+            b = Person.checkDisease(otherActor, self);
+            if(len(b)>0):
+                print("Infections from B to A:", b)
     def die(self):
-        self.world.population.remove(self)
+        self.alive = 0;
+        self.color = themeColors["dead"]
 class Disease:
     idct = 1;
-    diseaseList = []
     def __init__(self, world, virulence):
         self.id = Disease.idct;
         self.virulence = virulence;
         self.infected = 0;
         self.dead = 0;
         self.world = world;
+        self.history = {};
         Disease.idct+=1;
-        Disease.diseaseList.append(self);
+        world.diseaseList.append(self);
     def mutateVirulence(self):
         virulenceJitter = .05;
         self.virulence = self.virulence + random.uniform(-virulenceJitter, virulenceJitter)
@@ -107,11 +132,19 @@ def testPeople():
     ali.infect(cold)
     jordan.interact(ali)
 def main():
-    earth = World(100)
-    runTime = 100;
+    os.system("rm graphseries/*.png")
+    earth = World(1000)
+    runTime = 50;
     earth.tick()
     cold = Disease(earth, .8);
     earth.population[0].infect(cold)
+    earth.population[50].recover(cold, .95)
     for i in range(runTime):
         earth.tick()
-main()
+    print("Converting to GIF")
+    os.system("convert -delay 50 -loop 0 graphseries/*.png graphseries/network.gif")
+    print("Conversion complete")
+    os.system('xdg-open graphseries/network.gif')
+    return(earth.summary())
+history = main()
+plt.plot(history)
