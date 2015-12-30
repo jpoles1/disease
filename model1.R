46c334f618c92221fb922a4f38818fad60632a38
Jordan Poles <jpoles1@gmail.com>
2015-12-30 16:49:58 -0500

    Fixed up the R model1

--- model1.R
+++ model1.R
@@ -1,26 +1,35 @@
-#SIR Model of Disease Transmission
-Sinit = 1000
-Iinit = 10
-Rinit = 0
-S=Sinit
-I=Iinit
-R=Rinit
+#SIRD Model of Disease Transmission
+S=1000
+I=15
+R=0
 D=0
+beta=.0005
+gamma=.05
+mu=.02
+#Create History Dataframe
 history = data.frame(time=0, S=S,I=I,R=R,D=D);
-#history = data.frame()
-stepDisease = function(time, Beta, Gamma, Theta){
+#Create Time Step Function pmax used so values cannot drop below 0
+stepDisease = function(time, Beta, Gamma, Mu){
   newS = pmax(S - (Beta*I*S), 0)
-  newI = pmax(I + (Beta*I*S) - (Gamma*I) - (Theta*I), 0)
+  newI = pmax(I + (Beta*I*S) - (Gamma*I) - (Mu*I), 0)
   newR = pmax(R + (Gamma*I), 0)
-  newD = pmax(D + (Theta*I), 0)
+  newD = pmax(D + (Mu*I), 0)
   S <<- newS; I <<- newI; R <<- newR; D <<- newD;
   return(data.frame(time, S, I, R, D))
 }
+#Loop over step function
 nreps = 100
 for(i in 1:nreps){
-  history = rbind(history, stepDisease(i, .0005, .05, .02))
+  history = rbind(history, stepDisease(i, beta, gamma, mu))
 }
+#And finally plot
 require(reshape2)
 require(ggplot2)
 plotdat = melt(history, id.vars = c("time"))
-ggplot(data=plotdat, aes(time, value))+geom_line(aes(color=variable))
+ggplot(data=plotdat)+
+  aes(x=time, y=value, color=variable)+
+  geom_line()+
+  xlab("Time Step")+ylab("# Indv.")+
+  ggtitle(paste("SIRD Dynamics\nβ=",beta,"; γ=",gamma,"; μ=",mu,"\n", sep=""))+
+  scale_color_manual(name="Disease State", 
+    values=c("blue", "orange", "green", "red"))
