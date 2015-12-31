276dfda95eb8cc5b777cfc2699bd638224bea7ba
Jordan Poles <jpoles1@gmail.com>
2015-12-31 17:59:29 -0500

    Added images.

--- model1.py
+++ model1.py
@@ -1,10 +1,8 @@
 #Loading Libs
 import matplotlib.pyplot as plt
-%matplotlib inline  
 import pandas as pd
 from math import floor
 from ggplot import *
-import seaborn as sns
 #Initializing Vars
 S = 1000
 I = 15
@@ -14,7 +12,7 @@ steps = 50
 #Disease Parameters
 beta = .0005
 gamma = .05
-delta = .02
+mu = .02
 history = pd.DataFrame({"S": S, "I": I, "R": R, "D": D}, index=[0])
 #Run sim loop
 history["step"] = history.index
@@ -23,13 +21,13 @@ ggplot(plotData, aes(x="step", y="value", color="variable"))+geom_line()
 for step in range(1, steps):
     newInf = floor(min(max(beta*I*S, 0), S))
     newRec = floor(min(max(gamma*I, 0), I))
-    newDead = floor(min(max(delta*I, 0), I-newRec))
+    newDead = floor(min(max(mu*I, 0), I-newRec))
     S = S - newInf
     I = I + newInf - newRec - newDead
     R = R + newRec
     D = D + newDead
     history = history.append(pd.DataFrame({"S": S, "I": I, "R": R, "D": D}, index=[step]))
 history["step"] = history.index
+#Plot using Python port of ggplot
 plotData = pd.melt(history, id_vars=["step"], value_vars=["S","I","R","D"])
-sns.
 ggplot(plotData, aes(x="step", y="value", color="variable"))+geom_line()+xlab("Time Step")+ylab("# Hosts")
